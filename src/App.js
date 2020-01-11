445fb828709d14892cebc7a67557249c86b7854d
stijnblokker <stijnblokker@gmail.com>
2020-01-12 00:59:16 +0100

    create different pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useState } from 'react';
 import './index.css';
 
 import SearchBar from './components/SearchBar'
@@ -6,9 +6,10 @@ import SearchResult from './components/SearchResult'
 import Playlist from './components/Playlist'
 
 export const music = createContext('music')
+export const page = createContext('page')
 
-const initialList = null
-const listReducer = (state, action) => {
+const initialMusicList = null
+const musicListReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_ARTISTS':
       return action.payload.map(artist => {
@@ -23,26 +24,40 @@ const listReducer = (state, action) => {
     case 'SELECT_ARTISTS':
       return state.map(artist => artist.id === action.payload ? { ...artist, selected: !artist.selected } : artist)
     case 'ADD_VIDEO':
-      const {id, videos} = action.payload
-      console.log('id', id);
-      console.log('videos', videos);
-      return state.map(artist => artist.id === id ? { ...artist, videoLink: videos.map((video) => video.id) } : artist)
-    case 'SELECT_VIDEO':
-    // return state.map ( artist => artist.id === action.payload ? { ...artist, videoSelected: !artist.videoSelected } : artist)
+      const { id, videos } = action.payload
+      return state.map(artist => artist.id === id ? artist.videoLink !== 0 ? { ...artist, videoLink: videos.map((video) => video.id) } : artist : artist)
+    default:
+      return state
+  }
+}
+
+const initialPage = { search: true, similar: false, video: false }
+const pageReducer = (state, action) => {
+  switch (action.type) {
+    case 'SEARCH':
+      return { search: true, similar: false, video: false }
+    case 'SIMILAR':
+      return { search: false, similar: true, video: false }
+    case 'VIDEO':
+      return { search: false, similar: false, video: true }
     default:
       return state
   }
 }
 
 const App = () => {
-  const [musicList, DispatchMusicList] = useReducer(listReducer, initialList)
+  const [musicList, dispatchMusicList] = useReducer(musicListReducer, initialMusicList)
+  const [page, dispatchPage] = useReducer(pageReducer, initialPage)
+  console.log(page);
 
   return (
-    <music.Provider value={DispatchMusicList}>
-      <SearchBar />
-      <SearchResult musicList={musicList} />
-      <Playlist musicList={musicList} />
+    // <page.Provider value={dispatchPage}>
+    <music.Provider value={dispatchMusicList}>
+      {page.search && <SearchBar page={dispatchPage} />}
+      {page.similar && <SearchResult musicList={musicList} page={dispatchPage} />}
+      {page.video && <Playlist musicList={musicList} page={dispatchPage} />}
     </music.Provider>
+    // </page.Provider>
   );
 }
 
